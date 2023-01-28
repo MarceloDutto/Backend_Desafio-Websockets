448cdf43e8d1599c43326c868088e13fe396b1b9
Marcelo Dutto <marcelodutto@hotmail.com>
2023-01-28 16:22:41 -0300

    Desafío de Websockets con renderizado en tiempo real

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,9 +1,37 @@
-const socket = io()
+const socket = io();
+
+const renderProducts = (data) => {
+    let productsContainer = document.querySelector("#products-container");
+    productsContainer.innerHTML = '';
+
+    if(data.length === 0) {
+        productsContainer.innerHTML = '<p>¡Lo sentimos! Temporalmente no podemos mostrate nuestros productos. Regresa más tarde.</p>'
+    } else {
+
+        data.forEach(product => {
+            let productDiv = document.createElement('div');
+            
+            let divContent = `
+            <p>Nombre: ${product.title}</p>
+            <p>Descripción: ${product.description}</p>
+            <p>Categoría: ${product.category}</p>
+            <p>Precio: ${product.price}</p>
+            <p>Stock: ${product.stock}</p>
+            <p>ID: ${product.id}</p>
+            <p>Código de producto: ${product.code}</p>`
+    
+            productDiv.innerHTML = divContent;
+    
+            productsContainer.appendChild(productDiv);
+        });
+    }
+}
 
 socket.on('newProduct', data => {
-    console.log(data);
+    renderProducts(data);
 })
 
+
 socket.on('productDeleted', data => {
-    console.log(data);
+    renderProducts(data);
 })
